c47fe85b13c6476cf3afcaa1391d318da4118e09
aitorzubiri <aitorgaillard99@gmail.com>
2025-09-25 14:32:33 +0200

    change code to fail tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def dar_vuelta_texto(texto):
-        if texto == "":
-                return ""
-        else:
+        # if texto == "":
+                # return ""
+        #else:
             salida = texto[-1]
             for i in range(1, len(texto)):
                     salida = salida + (texto[-i-1])
