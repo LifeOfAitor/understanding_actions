a1b5f7bf827f49a986e27979c898149e84a3de12
aitorzubiri <aitorgaillard99@gmail.com>
2025-09-29 14:25:39 +0200

    correct code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def dar_vuelta_texto(texto):
-        return texto
+        return texto[::-1]
 
 if __name__ == "__main__":
         dar_vuelta_texto(input("Introduce texto para darle la vuelta: "))
